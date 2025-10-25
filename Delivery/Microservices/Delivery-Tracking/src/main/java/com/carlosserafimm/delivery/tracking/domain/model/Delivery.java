2b678d09e9aa5dbf6599c6aa2aba8fc24519898b
CarlosSerafimm <cubo_537@hotmail.com>
2025-10-24 22:34:47 -0300

    Create initial entity models for tracking service

---
+++ Delivery/Microservices/Delivery-Tracking/src/main/java/com/carlosserafimm/delivery/tracking/domain/model/Delivery.java
@@ -0,0 +1,52 @@
+package com.carlosserafimm.delivery.tracking.domain.model;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@Setter(AccessLevel.PRIVATE)
+@Getter
+public class Delivery {
+
+    @EqualsAndHashCode.Include
+    private UUID id;
+    private UUID courierId;
+
+    private DeliveryStatus status;
+
+    private OffsetDateTime placedAt;
+    private OffsetDateTime assignedAt;
+    private OffsetDateTime expectedDeliveredAt;
+    private OffsetDateTime fulfilledAt;
+
+    private BigDecimal distanceFee;
+    private BigDecimal courierPayout;
+    private BigDecimal totalCost;
+
+    private Integer totalItems;
+
+    private ContactPoint sender;
+    private ContactPoint recipient;
+
+    private List<Item> items = new ArrayList<Item>();
+
+
+    public static Delivery draft(){
+        Delivery delivery = new Delivery();
+        delivery.setId(UUID.randomUUID());
+        delivery.setStatus(DeliveryStatus.DRAFT);
+        delivery.setTotalItems(0);
+        delivery.setTotalCost(BigDecimal.ZERO);
+        delivery.setDistanceFee(BigDecimal.ZERO);
+        delivery.setCourierPayout(BigDecimal.ZERO);
+        return delivery;
+    }
+
+    public List<Item> getItems() {
+        return Collections.unmodifiableList(items);
+    }
+}
---
+++ Delivery/Microservices/Delivery-Tracking/src/main/java/com/carlosserafimm/delivery/tracking/domain/model/Item.java
@@ -0,0 +1,25 @@
+package com.carlosserafimm.delivery.tracking.domain.model;
+
+import lombok.*;
+
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Setter(AccessLevel.PRIVATE)
+@Getter
+public class Item {
+
+    @EqualsAndHashCode.Include
+    private UUID id;
+    private String name;
+    private Integer quantity;
+
+    static Item brandNew(String name, Integer quantity) {
+        Item item = new Item();
+        item.setId(UUID.randomUUID());
+        item.setName(name);
+        item.setQuantity(quantity);
+        return item;
+    }
+}
