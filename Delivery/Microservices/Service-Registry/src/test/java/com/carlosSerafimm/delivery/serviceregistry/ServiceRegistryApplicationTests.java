23ea75ca44c706aef930e5111f70ea043b489a28
CarlosSerafimm <cubo_537@hotmail.com>
2025-11-16 23:43:40 -0300

    feat: adding Eureka service discovery

---
+++ Delivery/Microservices/Service-Registry/src/test/java/com/carlosSerafimm/delivery/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosSerafimm.delivery.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
